697870410bfaab58fd597675af1e0aaa215aabf5
majkl36 <majk.cengel@gmail.com>
2025-09-08 20:49:14 +0200

    Update platformio.ini and src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+static const int PWM_PIN = 1;
+static const int PWM_CHANNEL = 0;
+static const int PWM_FREQ = 20000;
+static const int PWM_RESOLUTION = 11;
+static const int MAX_PWM_VALUE = (1 << PWM_RESOLUTION) - 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(PWM_PIN, PWM_CHANNEL);
+  // TC4426A is inverting -> MAX_PWM_VALUE = ON, 0 = OFF
+  ledcWrite(PWM_CHANNEL, MAX_PWM_VALUE); //start with LED OFF
+  Serial.println("LEDC: 20kHz, 11bit, inverted logic.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int i = 0; i <= MAX_PWM_VALUE; i += 8) {
+    int inv_pwm = MAX_PWM_VALUE - i; // invert PWM value for TC4426A
+    ledcWrite(PWM_CHANNEL, inv_pwm);
+    delay(2);
+  }
+  for (int i = MAX_PWM_VALUE; i >= 0; i -= 8) {
+    int inv_pwm = MAX_PWM_VALUE - i; // invert PWM value for TC4426A
+    ledcWrite(PWM_CHANNEL, inv_pwm);
+    delay(2);
+  }
 }
